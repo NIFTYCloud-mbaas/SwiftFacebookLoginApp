bce2ef0d43b5e7083fa4a2c8f7e5e4c2ec4f4da5
TamHH <vfa.tamhh@gmail.com>
2018-09-20 23:31:53 +0700

    delete pod folder

--- Podfile
+++ Podfile
@@ -1,7 +1,12 @@
 # Uncomment this line to define a global platform for your project
 platform :ios, '8.0'
 # Uncomment this line if you're using Swift
-use_frameworks!
+ use_frameworks!
+
 target 'SwiftFacebookLoginApp' do
-    pod 'NCMB', :git => 'https://github.com/natsumo/ncmb_ios.git'
+    pod 'Bolts', '~> 1.7.0'
+    pod 'FBSDKCoreKit', '~>4.10.1'
+    pod 'FBSDKLoginKit', '~>4.10.1'
+    pod 'NCMB', :git => 'https://github.com/NIFCloud-mbaas/ncmb_ios.git'
 end
+
--- SwiftFacebookLoginApp/LoginViewController.swift
+++ SwiftFacebookLoginApp/LoginViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import NCMB
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
     // label
@@ -18,30 +20,61 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            self.performSegueWithIdentifier("login", sender: self)
+        }
+    }
+    
     // Loginボタン押下時の処理
     @IBAction func FacebookLoginBtn(sender: AnyObject) {
         // labelを空にする
         self.label.text = ""
-        
-        NCMBFacebookUtils.logInWithReadPermission(["email"]) {(user, error) -> Void in
-            if (error != nil){
-                if (error.code == NCMBErrorFacebookLoginCancelled){
-                    // Facebookのログインがキャンセルされた場合
-                    print("Facebookのログインがキャンセルされました")
-                    self.label.text = "Facebookのログインがキャンセルされました"
-                        
-                }else{
-                    // その他のエラーが発生した場合
-                    print("エラーが発生しました：\(error.code)")
-                    self.label.text = "エラーが発生しました：\(error.code)"
-                        
+        if (FBSDKAccessToken.currentAccessToken() != nil) {
+            self.performSegueWithIdentifier("login", sender: self)
+        } else {
+            let fbManager = FBSDKLoginManager()
+            let permission = ["email", "public_profile"]
+            
+            fbManager.logInWithReadPermissions(permission, fromViewController: self, handler: { (result, error) in
+                if (error != nil){
+                    if(result.isCancelled){
+                        // Facebookのログインがキャンセルされた場合
+                        print("Facebookのログインがキャンセルされました")
+                        self.label.text = "Facebookのログインがキャンセルされました"
+                    } else {
+                        // その他のエラーが発生した場合
+                        print("エラーが発生しました：\(error.code)")
+                        self.label.text = "エラーが発生しました：\(error.code)"
+                    }
+                } else {
+                    // 会員登録とログイン後の処理
+                    if(result.token.userID != nil){
+                        let facebookInfo:NSDictionary = [
+                            "id":result.token.userID,
+                            "access_token":result.token.tokenString,
+                            "expiration_date":result.token.expirationDate
+                        ]
+                        let user = NCMBUser()
+                        user.signUpWithFacebookToken(facebookInfo as [NSObject : AnyObject], withBlock: { (error) in
+                            if(error != nil){
+                                //会員登録に失敗した場合の処理
+                                print("Facebookの会員登録とログインに成功しました：\(user.objectId)");
+                            } else {
+                                //会員登録に成功した場合の処理
+                                print("Facebookの会員登録とログインに成功しました：\(user.objectId)");
+                                self.performSegueWithIdentifier("login", sender: self)
+                            }
+                        })
+                    } else {
+                        print("エラーが発生しました：\(error.code)")
+                        self.label.text = "エラーが発生しました：\(error.code)"
+                    }
                 }
-            }else{
-                // 会員登録とログイン後の処理
-                print("Facebookの会員登録とログインに成功しました：\(user.objectId)")
-                self.performSegueWithIdentifier("login", sender: self)
-                    
-            }
+                
+            })
+            
         }
         
     }
